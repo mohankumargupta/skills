ef36cc9cbc99118bacf0699aa7fb93eb1c2e2f38
Mohan Gupta <raspberrypisig@gmail.com>
2026-04-28 12:39:50 +1000

    discover-crate: now using src along with examples

--- discover-crate/scripts/check_examples.sh
+++ discover-crate/scripts/check_examples.sh
@@ -5,17 +5,21 @@ GH_REPO="$1"
 #OWNER="${GH_REPO%%/*}"
 REPOSITORY="${GH_REPO#*/}"
 
-if [ -z "$REPOSITORY" ]; then
+#git clone --no-checkout --filter=blob:none https://github.com/marti157/bmp085-180-rs
+#git sparse-checkout set --no-cone  "Cargo.toml" "src/" "examples/"
+#git checkout
+
+if [ -z "$GH_REPO" ]; then
     echo "Usage: $0 owner/repo"
     exit 1
 fi
 
-RESULT=$(curl -s -o /dev/null -w "%{http_code}" "https://api.github.com/repos/$GH_REPO/contents/examples")
+GITHUB_REPOSITORY="https://github.com/${GH_REPO}"
+mkdir -p "artifacts"
+cd artifacts
+git clone --no-checkout --filter=blob:none "$GITHUB_REPOSITORY"
+cd "$REPOSITORY"
+git sparse-checkout set --no-cone  "Cargo.toml" "src/" "examples/"
+git checkout
 
-if [ "$RESULT" == "200" ];
-then
-  mkdir -p "artifacts/$REPOSITORY/examples"
-  npx -y tiged --force "${GH_REPO}/examples" "artifacts/$REPOSITORY/examples" || exit 1
-else
-exit 1
-fi
+
